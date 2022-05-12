4ae39e3c0467a6c8c93130e5e4dae394b5818387
Rongrong Chai <rchai@sagebase.org>
2022-05-12 15:54:21 -0400

    add test for dca_buttons

--- tests/testthat/test-dca_buttons.R
+++ tests/testthat/test-dca_buttons.R
@@ -1,3 +1,53 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("shinyButton Default", {
+
+  btn <- shinyButton(
+    "test-id", "test-button",
+    style = "color: red;"
+  )
+
+  expect_identical(btn[[1]]$attribs$id, "test-id")
+  expect_identical(btn[[1]]$attribs$class, "btn btn-default action-button dca-shiny-btn")
+  expect_identical(btn[[1]]$attribs$style, "color: red;")
+  expect_identical(btn[[2]]$stylesheet, "main.min.css")
+})
+
+test_that("arrowButton Default", {
+
+  btn <- arrowButton(
+    "test-id",
+    style = "color: red;"
+  )
+  expect_identical(btn[[1]]$attribs$id, "test-id")
+  expect_identical(purrr::map_chr(btn[[1]]$children[[1]][[1]], ~.x$attribs$class),
+                   rep("fa fa-angle-left", 3))
+  expect_identical(btn[[1]]$attribs$class, "btn btn-default action-button dca-left-btn")
+  expect_identical(btn[[1]]$attribs$style, "color: red;")
+  expect_identical(btn[[2]]$stylesheet, "main.min.css")
+})
+
+test_that("mediaButton Default", {
+
+  btn <- mediaButton(
+    "google",
+    style = "color: red;"
+  )
+
+  expect_true(grepl("fa-google", btn[[1]]$children[[1]]$attribs$class))
+  expect_true(is.null(btn[[1]]$attribs$link))
+  expect_identical(btn[[1]]$attribs$class, "dca-icon-btn")
+  expect_identical(btn[[1]]$attribs$style, "color: red;")
+  expect_identical(btn[[2]]$stylesheet, "main.min.css")
+})
+
+test_that("paletteButton Default", {
+
+  btn <- paletteButton(
+    "test-id", "red",
+    style = "color: red;"
+  )
+  expect_identical(btn[[1]]$attribs$id, "test-id")
+  expect_identical(btn[[1]]$attribs$class, "action-button dca-palette-btn")
+  expect_identical(btn[[1]]$children[[1]]$attribs$style, "color: red;")
+  expect_identical(btn[[1]]$attribs$style, "color: red;")
+  expect_identical(btn[[2]]$stylesheet, "main.min.css")
 })
